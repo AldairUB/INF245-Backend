eb9cb19957f1f38769138814187df072cae8bcbb
lloydcas <castillo.le@pucp.edu.pe>
2022-09-26 21:48:02 -0500

    Actualizaciones

--- src/main/java/pe/edu/pucp/dovah/Gestion/repository/FacultadRepository.java
+++ src/main/java/pe/edu/pucp/dovah/Gestion/repository/FacultadRepository.java
@@ -7,7 +7,12 @@
 package pe.edu.pucp.dovah.Gestion.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pe.edu.pucp.dovah.Gestion.model.Facultad;
 
+import java.util.List;
+
 public interface FacultadRepository extends JpaRepository<Facultad,Integer>{
+    @Query(value = "SELECT a FROM Facultad a WHERE a.activo = true")
+    List<Facultad> listarActivos();
 }
