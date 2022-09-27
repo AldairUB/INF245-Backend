182c47c4a634cee87252bd67d68faa7691a87385
csantv <csantve@gmail.com>
2022-09-26 21:30:56 -0500

    habilidad para subir documento v2

--- src/main/java/pe/edu/pucp/dovah/asignaciones/repository/TareaRepository.java
+++ src/main/java/pe/edu/pucp/dovah/asignaciones/repository/TareaRepository.java
@@ -11,6 +11,10 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pe.edu.pucp.dovah.asignaciones.model.Tarea;
 
-public interface TareaRepository extends JpaRepository<Tarea, Long> {
+import java.util.Optional;
 
+public interface TareaRepository extends JpaRepository<Tarea, Long> {
+    @EntityGraph(attributePaths = {"listaDocumentos"})
+    @Override
+    Optional<Tarea> findById(Long id);
 }
